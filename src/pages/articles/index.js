2c52776cdb7af58d125819ce041a58f1cbafaf8e
ajaysharvesh3 <ajaysharvesh.mp@gmail.com>
2023-12-22 21:12:48 +0530

    modification 4

--- src/pages/articles/index.js
+++ src/pages/articles/index.js
@@ -1,11 +1,4 @@
 import { Inter } from "next/font/google";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-import Navbar from "@/components/navbar";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import Footer from "@/components/footer";
-import axios from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
